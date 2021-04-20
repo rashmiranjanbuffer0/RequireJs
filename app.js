23759d7922cc6f1304e39fec4dec8a1a8f2a641e
Rashmi Ranjan <rashmiranjan.bufferzero@gmail.com>
2021-04-20 14:48:13 +0530

    carousel added

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 requirejs.config({
     baseUrl: 'scripts',
     paths: {
-        popper: ['https://unpkg.com/@popperjs/core@2', 'js/popper.js'],
+        popperjs: 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min',
         jquery: ['https://code.jquery.com/jquery-3.6.0.min' , 'jquery.min'],
-        bootstrap: ['https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.m in' , '/bootstrap/js/bootstrap.min']
+        bootstrap: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min' ,
     },
-    shim: {
-        "jquery.bootstrap": {
-            deps: ["jquery"]
-        }
-    }
-}); 
+});
+require(['popperjs','bootstrap'], function(){
+    console.log("require function is called.");
+});
